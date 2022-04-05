e7ba11b83a45cf079ff4f70feaab8a44c16324ce
Praneeth <malpraneeth@deloitte.com>
2022-04-05 20:02:48 +0530

    Mini Assignment3

---
+++ Assignments/MiniAssignment3.py
@@ -0,0 +1,67 @@
+from collections import Counter
+# count duplicates in list
+def duplicateCount(array):
+    for element in array:
+        dictionary = Counter(element)
+        for keyvalue in dictionary:
+            if dictionary[keyvalue] > 1:
+                print(keyvalue, "--->", dictionary[keyvalue])
+l = [[1, 1, 3, 2], [9, 8, 8, 1], [0, 4, 5, 0, 0, 4]]
+print("It wll returns all the duplicate values from list of array list ")
+duplicateCount(l)
+print()
+
+#Merging two lists
+list1 = ["Hello ", "take "]
+list2 = ["Dear", "Sir"]
+output_list = []
+for i in range(len(list2)):
+    for j in range(len(list1)):
+        s = list1[i]+list2[j]
+        output_list.append(s)
+print(output_list)
+
+#adding subList to list
+print("extending by adding subList")
+nested_list = ["a", "b", ["c", ["d", "e", ["f", "g"], "k"], "l"], "m", "n"]
+sub_list = ["h", "i", "j"]
+nested_list[2][1][2].extend(sub_list)
+print(nested_list)
+
+#Merging two dictionaries
+dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+dict3 = dict1.copy()
+dict3.update(dict2)
+print(dict3)
+
+#Renaming Key in dictionary
+sampleDict =\
+    {
+  "name": "Kelly",
+  "age": 25,
+  "salary": 8000,
+  "city": "New york"
+}
+sampleDict["location"] = sampleDict.pop("city")
+print(sampleDict,'\n')
+
+# Convert Dictionary to list
+Dict = {"HuEx": [1, 3, 4],
+        "is": [7, 6],
+        "best": [4, 5]
+        }
+resultList = []
+for key in Dict:
+    temp_list = []
+    temp_list.append(key)
+    for i in Dict[key]:
+        temp_list.append(i)
+    resultList.append(temp_list)
+print(resultList)
+
+
+
+
+
+
