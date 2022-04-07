f3db75bad10ee9bae94658a89d23fae5065e92c2
Praneeth <malpraneeth@deloitte.com>
2022-04-08 02:24:44 +0530

    Main Assignment

---
+++ MainAssignment/WelcomePage.py
@@ -0,0 +1,38 @@
+from AdminPage import adminAction
+
+
+class Welcome:
+    def welcome(self):
+        print("******Welcome to BookMyShow*******\n1.Login\n2.Register\n3.Exit")
+
+
+def option():
+    option = int(input("enter option: "))
+    return option
+
+
+def main():
+    admin_dic = {
+        'pass': 'admin123'
+    }
+    while True:
+        # created obj of welcome class
+        wel = Welcome()
+        wel.welcome()
+        opt = option()
+        if opt == 1:
+            print("******Welcome to BookMyShow*******")
+            user = input("enter login id: ")
+            password = input("enter password:")
+            if user == 'Admin':
+                if admin_dic['pass'] == password:
+                    adminAction()
+        elif opt == 2:
+            print("****Create new Account***** ")
+
+        else:
+            break
+
+
+if __name__ == "__main__":
+    main()
